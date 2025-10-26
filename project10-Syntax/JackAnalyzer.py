68b0a9501fe0e8cac303e6fc53a68d8b948fa163
Paul Hansen <paul.c.hansen@gmail.com>
2025-10-25 18:44:14 -0700

    start testing handle_paths for JackAnalyzer

--- project10-Syntax/JackAnalyzer.py
+++ project10-Syntax/JackAnalyzer.py
@@ -1,8 +1,57 @@
-# Hello
+import argparse
+from os import listdir
+import pathlib
+from typing import List, Optional, Tuple
 
 def main():
-    print("Hello, whyrld!")
 
+    parser = argparse.ArgumentParser("JackAnalyzer")
+    parser.add_argument(
+        "input",
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        "output",
+        type=pathlib.Path,
+        nargs="?"
+    )
+
+    args = parser.parse_args()
+
+
+    in_paths, out_paths = handle_paths(args.input, args.output)
+
+
+
+def handle_paths(
+    in_path: pathlib.Path,
+    out_path: Optional[pathlib.Path]
+) -> Tuple[List[pathlib.Path], List[pathlib.Path]]:
+    
+    if in_path.is_dir():
+        in_paths = [path for path in in_path.iterdir() if path.suffix == ".jack"]
+
+        if out_path is None:
+            # in_path is directory; put outputs in same directory
+            out_paths = [path.with_name(path.stem + "T").with_suffix(".xml") for path in in_paths]
+        else:
+            # in_path is directory; out_path is directory
+            assert isinstance(out_path, pathlib.Path)
+
+            if not out_path.exists:
+                out_path.mkdir(parents=True, exist_ok=True)
+
+            out_paths = [(out_path / (path.stem + "T")).with_suffix(".xml") for path in in_paths]
+
+        return in_paths, out_paths
+    else:
+        if out_path is None:
+            # in_path is file; put output next to it
+            out_path = in_path.with_name(in_path.stem + "T").with_suffix(".xml")
+        else:
+            out_path = out_path.with_name(in_path.stem + "T").with_suffix(".xml")
+        
+        return [in_path], [out_path]
 
 
 if __name__ == '__main__':
---
+++ project10-Syntax/tests/test_paths.py
@@ -0,0 +1,50 @@
+import pathlib
+import pytest
+import sys
+import rich
+
+from JackAnalyzer import handle_paths
+
+def test_input_directory(tmp_path: pathlib.Path):
+    """
+    Input is a directory with files in it
+    """
+
+    file1 = tmp_path / "file1.jack"
+    file2 = tmp_path / "file2.jack"
+    file3 = tmp_path / "file3.csv"
+
+    file1.write_text("aoeu")
+    file2.write_text("aoeu")
+    file3.write_text("aoeu")
+
+    in_paths, out_paths = handle_paths(tmp_path, None)
+
+    assert {p.name for p in in_paths} == {"file1.jack", "file2.jack"}
+    assert {p.name for p in out_paths} == {"file1T.xml", "file2T.xml"}
+
+def test_input_output_directory(tmp_path: pathlib.Path):
+    """
+    Input is a directory with files in it.
+    Output is a directory.
+    """
+
+    src_dir = tmp_path / "src"
+    src_dir.mkdir()
+
+    xml_dir = tmp_path / "xml"
+    xml_dir.mkdir()
+
+    file1 = src_dir / "file1.jack"
+    file2 = src_dir / "file2.jack"
+    file3 = src_dir / "file3.csv"
+
+    file1.write_text("aoeu")
+    file2.write_text("aoeu")
+    file3.write_text("aoeu")
+
+    in_paths, out_paths = handle_paths(src_dir, xml_dir)
+
+    assert {p.name for p in in_paths} == {"file1.jack", "file2.jack"}
+    assert {p.name for p in out_paths} == {"file1T.xml", "file2T.xml"}
+    assert {p.parent.stem for p in out_paths} == {"xml", "xml"}
