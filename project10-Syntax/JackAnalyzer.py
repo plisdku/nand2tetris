d102376bafbfd6144e1f62ba25e4a007a12aaa4f
Paul Hansen <paul.c.hansen@gmail.com>
2025-10-25 19:01:29 -0700

    move tokenizer and handle_paths into jack_analyzer module. add a default output directory just for now so i don't overwrite anything important.

--- project10-Syntax/JackAnalyzer.py
+++ project10-Syntax/JackAnalyzer.py
@@ -1,7 +1,7 @@
 import argparse
-from os import listdir
 import pathlib
-from typing import List, Optional, Tuple
+
+from jack_analyzer import handle_paths, tokenize
 
 def main():
 
@@ -13,45 +13,19 @@ def main():
     parser.add_argument(
         "output",
         type=pathlib.Path,
-        nargs="?"
+        nargs="?",
+        default="out"
     )
 
     args = parser.parse_args()
 
-
     in_paths, out_paths = handle_paths(args.input, args.output)
 
+    for _in, _out in zip(in_paths, out_paths):
+        _out.parent.mkdir(parents=True, exist_ok=True)
+        _out.write_text(tokenize(_in.read_text()))
 
 
-def handle_paths(
-    in_path: pathlib.Path,
-    out_path: Optional[pathlib.Path]
-) -> Tuple[List[pathlib.Path], List[pathlib.Path]]:
-    
-    if in_path.is_dir():
-        in_paths = [path for path in in_path.iterdir() if path.suffix == ".jack"]
-
-        if out_path is None:
-            # in_path is directory; put outputs in same directory
-            out_paths = [path.with_name(path.stem + "T").with_suffix(".xml") for path in in_paths]
-        else:
-            # in_path is directory; out_path is directory
-            assert isinstance(out_path, pathlib.Path)
-
-            if not out_path.exists:
-                out_path.mkdir(parents=True, exist_ok=True)
-
-            out_paths = [(out_path / (path.stem + "T")).with_suffix(".xml") for path in in_paths]
-
-        return in_paths, out_paths
-    else:
-        if out_path is None:
-            # in_path is file; put output next to it
-            out_path = in_path.with_name(in_path.stem + "T").with_suffix(".xml")
-        else:
-            out_path = out_path.with_name(in_path.stem + "T").with_suffix(".xml")
-        
-        return [in_path], [out_path]
 
 
 if __name__ == '__main__':
