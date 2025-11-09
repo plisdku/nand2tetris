fb86e6beae6d1732c03a242095889ae916ef590b
Paul Hansen <paul.c.hansen@gmail.com>
2025-11-09 14:00:34 -0800

    add class_var_dec tests, which pass

--- project11-Compiler/tests/test_compiler.py
+++ project11-Compiler/tests/test_compiler.py
@@ -42,6 +42,7 @@ def test_var_dec_char():
 
     assert c.local_symbols["alfalfa"] == Symbol("alfalfa", "var", "char", 0)
 
+
 def test_var_dec_class():
     jack = dedent("""
         var Unicorn xyzzy;
@@ -57,3 +58,35 @@ def test_var_dec_class():
 
     assert c.local_symbols["xyzzy"] == Symbol("xyzzy", "var", "Unicorn", 0)
 
+
+def test_class_var_dec_two_fields():
+    jack = dedent("""
+        field char ww, zz;
+    """)
+
+    c = Compiler(code=jack)
+    out = c.compile_class_var_dec()
+
+    # assert out is None
+
+    assert len(c.local_symbols) == 0
+    assert len(c.static_symbols) == 2
+
+    assert c.static_symbols["ww"] == Symbol("ww", "field", "char", 0)
+    assert c.static_symbols["zz"] == Symbol("zz", "field", "char", 1)
+
+def test_class_var_dec_static():
+
+    jack = dedent("""
+        static Orb round_guy;
+    """)
+
+    c = Compiler(code=jack)
+    out = c.compile_class_var_dec()
+
+    # assert out is None
+
+    assert len(c.local_symbols) == 0
+    assert len(c.static_symbols) == 1
+
+    assert c.static_symbols["round_guy"] == Symbol("round_guy", "static", "Orb", 0)
